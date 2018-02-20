36ff5cceb8f1ae58d9c87f2e23d1841d23dcc944
Andrewkha <chernyshov.andrey@gmail.com>
2018-02-20 18:05:15 +0300

    rbac role initialization added to data transfer console command

--- console/controllers/DataTransferController.php
+++ console/controllers/DataTransferController.php
@@ -9,6 +9,7 @@
 namespace console\controllers;
 
 
+use core\access\Rbac;
 use core\entities\DataTransfer\Country;
 use core\repositories\sf\CountryRepository;
 use yii\console\Controller;
@@ -16,22 +17,39 @@ use yii\console\Controller;
 class DataTransferController extends Controller
 {
     private $countries;
+    private $manager;
 
     public function __construct(string $id, $module, CountryRepository $countries, array $config = [])
     {
         parent::__construct($id, $module, $config);
         $this->countries = $countries;
+        $this->manager = \Yii::$app->authManager;
     }
 
     public function actionImport()
     {
-        $this->countriesData();
+
+        $this->stdout('Importing countries data' . PHP_EOL);
+            try {
+                $this->countriesData();
+            } catch (\Exception $e) {
+                $this->stdout($e->getMessage() . PHP_EOL);
+            }
+        $this->stdout('Done!' . PHP_EOL);
+
+        $this->stdout('Initializing RBAC config' . PHP_EOL);
+        $this->rbacInit();
+        $this->stdout('Done' . PHP_EOL);
+
     }
 
     private function countriesData()
     {
         $reader = new \XMLReader();
-        $reader->open('console/import/countries.xml');
+
+        if (!$reader->open('console/import/countries.xml')) {
+            throw new \RuntimeException('Can not open the source file');
+        }
 
         $country = [];
 
@@ -48,5 +66,21 @@ class DataTransferController extends Controller
         foreach ($country as $one) {
             $this->countries->save($one);
         }
+    }
+
+    private function rbacInit()
+    {
+        $am = $this->manager;
+        $am->removeAll();
+
+        $user = $am->createRole(Rbac::ROLE_USER);
+        $user->description = 'Regular site user';
+        $am->add($user);
+
+        $admin = $am->createRole(Rbac::ROLE_ADMIN);
+        $admin->description = 'Site administrator';
+        $am->add($admin);
+
+        $am->addChild($admin, $user);
     }
 }
